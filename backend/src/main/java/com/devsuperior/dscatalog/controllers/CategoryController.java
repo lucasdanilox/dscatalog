7fe421efa8402b8552bc3286c985a7acaa4ee334
lucasdanilox <lukastecno@gmail.com>
2023-12-27 18:33:28 -0300

    Exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,7 +1,6 @@
-package com.devsuperior.dscatalog.resources;
+package com.devsuperior.dscatalog.controllers;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
-public class CategoryResource {
+public class CategoryController {
 
     @Autowired
     private CategoryService service;
