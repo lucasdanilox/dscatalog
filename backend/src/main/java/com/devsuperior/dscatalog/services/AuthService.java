618740df36534939856fac276213d7affe2bf778
lucasdanilox <lukastecno@gmail.com>
2024-01-20 14:23:06 -0300

    Starting the first step of password recovery

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.dto.EmailDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+
+    public void createRevoverToken(EmailDTO body) {
+
+
+
+    }
+}
