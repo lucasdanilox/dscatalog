6b0e659d0ac2370a32e4fea2e7a638289742b40c
lucasdanilox <lukastecno@gmail.com>
2024-01-20 14:58:46 -0300

    EmailException exceptionHandler

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ControllerExceptionHandler.java
@@ -57,4 +57,17 @@ public class ControllerExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(EmailException.class)
+    public ResponseEntity<StandardError> database(EmailException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Email exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EmailException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EmailException.java
@@ -1,9 +1,8 @@
 package com.devsuperior.dscatalog.services.exceptions;
 
-@SuppressWarnings("serial")
 public class EmailException extends RuntimeException {
 
-	public EmailException(String msg) {
-		super(msg);
-	}
+    public EmailException(String msg) {
+        super(msg);
+    }
 }
