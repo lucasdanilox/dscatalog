6d5fdb25c0668dc7d1e6b4a8f65aa5da3e30aeed
lucasdanilox <lukastecno@gmail.com>
2024-01-20 14:52:20 -0300

    logic for sending the token by email

--- backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,15 +1,57 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.entities.PasswordRecover;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinute;
 
+    @Value("${email.password-recover.uri}")
+    private String recoverURI;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Transactional
     public void createRevoverToken(EmailDTO body) {
 
+        User user = userRepository.findByEmail(body.getEmail());
+        if (user == null) {
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
 
+        String token = UUID.randomUUID().toString();
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.getEmail());
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinute * 60L));
+        entity = passwordRecoverRepository.save(entity);
+
+        String text = "Acesse o link para redefinir sua senha\n\n"
+                + recoverURI + token + "\n Validade de " + tokenMinute + " minutos.";
+
+        emailService.sendEmail(body.getEmail(), "Recuperação de Senha", text);
 
     }
 }
